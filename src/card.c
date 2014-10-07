e44898b68b0e620f183b5743d23cc88972863b28
William Johnston <wjohnston@mpr.org>
2014-10-07 08:29:33 -0500

    Actually convert int to string.

--- src/card.c
+++ src/card.c
@@ -1,37 +1,21 @@
-// #include <stdlib.h>
+#include <stdio.h>
 #include "card.h"
 
 char * get_formatted_number(int number) {
+    char * out;
+
     switch (number) {
         case 0:
-            return "A";
+            out = "A";
         case 11:
-            return "J";
+            out = "J";
         case 12:
-            return "Q";
+            out = "Q";
         case 13:
-            return "K";
-        case 1:
-            return "1";
-        case 2:
-            return "2";
-        case 3:
-            return "3";
-        case 4:
-            return "4";
-        case 5:
-            return "5";
-        case 6:
-            return "6";
-        case 7:
-            return "7";
-        case 8:
-            return "8";
-        case 9:
-            return "9";
-        case 10:
-            return "10";
+            out = "K";
         default:
-            return number;
+            sprintf(out, "%d", number);
     }
+
+    return out;
 }
